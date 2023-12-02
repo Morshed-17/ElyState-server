45d636bc71faadcd7711942b2936964d25ce54cf
Morshed-17 <morshedmto@gmail.com>
2023-12-02 10:51:38 +0600

    all properties and single property endpoint added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
@@ -16,7 +16,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token;
@@ -35,81 +34,106 @@ const verifyToken = async (req, res, next) => {
 };
 
 const client = new MongoClient(process.env.DB_URI, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    },
-  })
-  async function run() {
-    try {
-      // auth related api
-      app.post('/jwt', async (req, res) => {
-        const user = req.body
-        console.log('I need a new jwt', user)
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '365d',
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const propertiesCollection = client
+      .db("ElyStateDB")
+      .collection("properties");
+
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("I need a new jwt", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
         })
+        .send({ success: true });
+    });
+
+    // Logout
+    app.get("/logout", async (req, res) => {
+      try {
         res
-          .cookie('token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
           })
-          .send({ success: true })
-      })
-  
-      // Logout
-      app.get('/logout', async (req, res) => {
-        try {
-          res
-            .clearCookie('token', {
-              maxAge: 0,
-              secure: process.env.NODE_ENV === 'production',
-              sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-            })
-            .send({ success: true })
-          console.log('Logout successful')
-        } catch (err) {
-          res.status(500).send(err)
-        }
-      })
-  
-      // Save or modify user email, status in DB
-      app.put('/users/:email', async (req, res) => {
-        const email = req.params.email
-        const user = req.body
-        const query = { email: email }
-        const options = { upsert: true }
-        const isExist = await usersCollection.findOne(query)
-        console.log('User found?----->', isExist)
-        if (isExist) return res.send(isExist)
-        const result = await usersCollection.updateOne(
-          query,
-          {
-            $set: { ...user, timestamp: Date.now() },
-          },
-          options
-        )
-        res.send(result)
-      })
-  
-      // Send a ping to confirm a successful connection
-      await client.db('admin').command({ ping: 1 })
-      console.log(
-        'Pinged your deployment. You successfully connected to MongoDB!'
-      )
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
+          .send({ success: true });
+        console.log("Logout successful");
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    // Save or modify user email, status in DB
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const isExist = await usersCollection.findOne(query);
+      console.log("User found?----->", isExist);
+      if (isExist) return res.send(isExist);
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      );
+      res.send(result);
+    });
+
+    //properties
+
+    app.get("/properties", async (req, res) => {
+      try {
+        const result = await propertiesCollection.find().toArray();
+        res.send(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+    app.get("/property/:id", async (req, res) => {
+      try {
+        const id = req.params
+        const query = {_id: new ObjectId(id)}
+        const result = await propertiesCollection.findOne(query);
+        res.send(result);           
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
-  run().catch(console.dir)
-  
-  app.get('/', (req, res) => {
-    res.send('Hello from ElyState Server..')
-  })
-  
-  app.listen(port, () => {
-    console.log(`ElyState is running on port ${port}`)
-  })
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from ElyState Server..");
+});
+
+app.listen(port, () => {
+  console.log(`ElyState is running on port ${port}`);
+});
