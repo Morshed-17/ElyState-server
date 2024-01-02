f6288e7986b1662869d26b7cffaf5dd09be83915
Morshed-17 <morshedmto@gmail.com>
2024-01-02 21:57:24 +0600

    MVC pattern

---
+++ MVC.pattern.js
@@ -0,0 +1,8 @@
+// MVC = model view controller
+
+// model = It manages the data, business rules, and logic of the application. The model is responsible for retrieving and storing data, as well as notifying observers (such as the view) about changes in the data.
+
+// view = This represents the user interface or presentation layer of the application. It is responsible for displaying the data to the user and sending user input to the controller.
+
+
+// controller = the brain or traffic cop of a software application. Its main job is to receive input from the user (via the view), process that input, interact with the model (which manages data and logic), and then update the view accordingly. 
